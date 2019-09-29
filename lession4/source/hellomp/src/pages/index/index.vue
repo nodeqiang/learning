5973c3075628f78a2478f2806fda67ba3540e7b7
wesleywang <9610848@qq.com>
2019-09-29 15:09:36 +0800

    lession 4

--- lession4/source/hellomp/src/pages/index/index.vue
+++ lession4/source/hellomp/src/pages/index/index.vue
@@ -1,11 +1,48 @@
 <template>
-  <div></div>
+  <div class="page">
+    <div class="row">
+      <input v-model="source" />
+      <button @click="result=sqrt(source)">确定</button>
+    </div>
+    <div class="result"> {{ result }} </div>
+  </div>
 </template>
 
 <script>
 module.exports = {
+  data: () => ({
+    source: 2,
+    result: ''
+  }),
+  methods: {
+    sqrt: require('sqrt2')
+  }
 }
 </script>
 
 <style scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+  align-items: stretch;
+  margin-top: 200rpx;
+  margin-bottom: 20rpx;
+}
+
+input {
+  border: 1px solid #8c8c8c;
+  padding: 10rpx;
+  text-align: center;
+  height: 50rpx;
+}
+
+button {
+  line-height: 2;
+}
+
 </style>
