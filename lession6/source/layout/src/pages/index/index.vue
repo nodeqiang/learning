54cb289833413e788c7acf46fe0506cdb0b716d5
wesleywang <9610848@qq.com>
2019-10-20 23:27:03 +0800

    add lesson6

---
+++ lession6/source/layout/src/pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="beijing border">
+      <div class="border" style="color:red;font-size:1.2em;">
+        牢里的恶劣空气，于连已不能忍受。幸亏他们通知他赴死的那一天，明媚的阳光使万物洋溢着欢乐，于连也浑身充满了勇气。在露天行走，给了他一种甜美的感觉，仿佛久在海上颠簸的水手登上陆地散步一样。“来吧，一切顺利，”
+        <span class="border">他对自己说，“我一点儿都不缺乏勇气。”</span>
+        <image src="/static/images/user.png" class="border" style="vertical-align:bottom;width:1em;height:1em;" />
+        <radio checked />
+        <checkbox checked />
+        <input placeholder="输入你的名字" style="padding:0.3em;" class="border"/>
+        <textarea placeholder="说点什么吧" style="padding:0.3em;" class="border"/>
+        <button>hello</button>
+        <div class="border" style="color:#00FF00;font-size:1.2em;padding:1em;margin:1em 0.1em 2em 0.1em;">这颗头颅从不曾像将要落地时那么富有诗意。从前他在韦尔吉的树林里度过的那些最温馨的时刻纷至沓来，极其有力地涌上他的脑际。
+          <div style="color:rgb(0,0,255);font-size:1.2em;margin-top: 1em;" class="border">一切都进行得简单、得体。
+            <div style="color:rgba(0,0,255,0.5);font-size:1rem;" class="border">两天前，他曾对富凯说：</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+}
+</script>
+
+<style scoped>
+div {
+  box-sizing: border-box;
+}
+.border {
+  border:1px solid black;
+}
+.beijing {
+  width:100%;
+  height:100vh;
+}
+</style>
